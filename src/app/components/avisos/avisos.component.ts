a1593c103e3712100d03d9dedfd05e32843ed1e4
Miguel <miguelhurtado@MBP-Migue-2.home>
2020-10-08 14:15:16 +0200

    Completamos parte del componente avisos para que muestre un mensaje y luego lo oculte.

--- src/app/components/avisos/avisos.component.ts
+++ src/app/components/avisos/avisos.component.ts
@@ -1,3 +1,4 @@
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AvisosComponent implements OnInit {
 
-  constructor() { }
+  mensaje: string;  //creamos las variables
+  visible: boolean;
+
+  constructor() {    //nuestros constructores
+    this.mensaje = "Correo enviado";
+    this.visible = false;
+   }
 
   ngOnInit(): void {
+    this.showMenssage('Correo Enviado'); //al recargar la pagina llamamos a esta funcion
   }
 
+  showMenssage(mensaje: string){ //Funcion a la que llamamos para mostrar mesaje
+    this.mensaje = mensaje;
+    this.visible = true;
+    this.waitToHide();
+  }
+
+  hideMenssage(){  //Funcion a la que llamamos para esconder el mensaje
+    this.visible = false;
+    this.mensaje = '';
+  }
+
+  waitToHide(){ //Funcion para que nos mantenga el mensaje solo por un tiempo y luego se quite.
+    setTimeout(() => {
+      this.hideMenssage();
+    }, 2000);
+  }
 }
